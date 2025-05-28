b11579f48ca34d7b0e54fd519c2469c5ec3bb090
abu-tahir-sk <abutahir2626@gmail.com>
2025-05-27 17:45:15 -0900

    added balance

--- js/home.js
+++ js/home.js
@@ -13,4 +13,29 @@ console.log(addMoneyInput);
 // get the pin number provided
 const pinNumberInput = document.getElementById('input-pin-number').value;
 console.log(pinNumberInput);
+
+// step-3: verify pin number 
+// wrong way to validate pin number
+if(pinNumberInput === '1234'){
+  console.log('adding money to your account')
+
+  // step-4: get the current balance
+  const balance = document.getElementById('account-balance').innerText;
+  console.log(balance);
+
+  // step-5: add addMoneyInput with balance
+  // eta korle hobe na 
+  // const newBalance = balance + addMoneyInput; // eta akta string
+  const addMoneyNumber = parseFloat(addMoneyInput);
+  const balanceNumber = parseFloat(balance);
+  const newBalance = addMoneyNumber + balanceNumber;
+  console.log(newBalance);
+
+  // step-6 : update the balance in the UI/DOM
+  document.getElementById('account-balance').innerText = newBalance;
+
+}
+else{
+  alert('Failed to add money please try again')
+}
 });
